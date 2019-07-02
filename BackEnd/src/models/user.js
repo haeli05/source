5a2df1d96b01e2c1787799ce9e0c4550e6754c66
netsudo <netsudo@users.noreply.github.com>
2019-07-02 14:52:29 -0700

    Hashing the user password before it's saved

--- BackEnd/src/models/user.js
+++ BackEnd/src/models/user.js
@@ -1,45 +1,56 @@
-import validator from 'validator';
-import bcrypt from 'bcrypt';
+const validator = require('validator');
+const bcrypt    = require('bcrypt');
 
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-	  name: {
-		  type: DataTypes.STRING, 
-		  validate: {
-			  // Check name only contains characters
-			  is: ["^[a-z]+$",'i'],
-			  len: [1,30]
-		  },
-		  allowNull: false
-	  },
-	  username: {
-		  type: DataTypes.STRING, 
-		  validate: {
-			  len: [2,16]
-		  },
-		  allowNull: false,
-		  unique: true
-	  },
-	  email: {
-		  type: DataTypes.STRING, 
-		  validate: {
-			  isEmail: true,
-			  max: 50
-		  },
-		  allowNull: false,
-		  unique: true
-	  },
-	  password: {
-		  type: DataTypes.STRING,
-		  validate: {
-			  len: [8,42]
-		  },
-		  allowNull: false
-	  },
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+	const User = sequelize.define('User', {
+		name: {
+			type: DataTypes.STRING, 
+			validate: {
+				// Check name only contains characters
+				is: ["^[a-z]+$",'i'],
+				len: [1,30]
+			},
+			allowNull: false
+		},
+		username: {
+			type: DataTypes.STRING, 
+			validate: {
+				len: [2,16]
+			},
+			allowNull: false,
+			unique: true
+		},
+		email: {
+			type: DataTypes.STRING, 
+			validate: {
+				isEmail: true,
+				max: 50
+			},
+			allowNull: false,
+			unique: true
+		},
+		password: {
+			type: DataTypes.STRING,
+			validate: {
+				len: [8,42]
+			},
+			allowNull: false
+		},
+	}, {});
+
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+
+	// Hashing password before saving it to the database
+	User.beforeCreate((user, options) => {
+		bcrypt.hash(user.password, 10, function (err, hash) {
+			if (err) {
+				return next(err);
+			}
+			user.password = hash;
+		});
+	});
+	return User;
 };
