cef2ec4e700a3206160b36ec8e68f21f33fad5ba
netsudo <netsudo@users.noreply.github.com>
2019-07-09 00:24:59 -0700

    Initial project model sans relationships

---
+++ BackEnd/src/models/project.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Project = sequelize.define('Project', {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    board: DataTypes.JSON,
+    contactMethod: DataTypes.ARRAY(DataTypes.STRING)
+  }, {});
+  Project.associate = function(models) {
+    // associations can be defined here
+  };
+  return Project;
+};
