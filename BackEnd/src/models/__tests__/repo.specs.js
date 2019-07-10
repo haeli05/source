6324c1f736e59ecd5f67827b0361840d3c0665e8
netsudo <netsudo@users.noreply.github.com>
2019-07-09 21:29:38 -0700

    Cleaned out old tests, changed relationship for projects and added initial test for what mocha will use

--- BackEnd/src/models/__tests__/repo.specs.js
+++
@@ -1,7 +0,0 @@
-const mongoose =require('mongoose');
-var mongoDB = 'mongodb://localhost/source';
-mongoose.connect(mongoDB);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-var db = mongoose.connection;
--- BackEnd/src/models/project.js
+++ BackEnd/src/models/project.js
@@ -1,13 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Project = sequelize.define('Project', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    board: DataTypes.JSON,
-    contactMethod: DataTypes.ARRAY(DataTypes.STRING)
-  }, {});
-  Project.associate = function(models) {
-    // associations can be defined here
-  };
-  return Project;
+	const Project = sequelize.define('Project', {
+		name: DataTypes.STRING,
+		description: DataTypes.STRING,
+		board: DataTypes.JSON,
+		contactMethod: DataTypes.ARRAY(DataTypes.STRING)
+	}, {});
+	Project.associate = function(models) {
+		// associations can be defined here
+		Project.belongsTo(User, {as: 'creator'});
+	};
+	return Project;
 };
