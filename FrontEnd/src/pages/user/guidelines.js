217ea7d0002d4b8e95c9d2792c799248f1947b94
Hao Jun Tan <havoc795@hotmail.com>
2019-09-11 15:31:04 -0400

    guidelines styling

--- FrontEnd/src/pages/user/guidelines.js
+++ FrontEnd/src/pages/user/guidelines.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
-
+import Link from '@material-ui/core/Link';
 
 
 export default class Guidelines extends Component {
   constructor (props) {
+    super(props)
   }
   render () {
     return (
-      <div className="Section">
-      <Typography variant='h1'>Guidelines</Typography>
+      <div className="StdSection">
+        <Typography variant='h1'>Guidelines</Typography>
+        <br /> <br />
+        source connects developers to projects and tasks. We work best when both parties can collaborate productively.
+        <br />
+        This document outlines the general guidelines intended to help projects and developers work together effectively, and resolve conflicts should they arise.
+        <br /><br />
+        <br /><br />
+        <Typography variant='h3'>Contribution Guidelines</Typography>
+        <br />
+        <Typography variant="h4">For Project Managers:</Typography>
+        1. Specify the scope of tasks, and rewards associated clearly<br />
+        2. Respond to interested parties clearly. Be respectful of their time.<br />
+        3. Follow up on work in progress to clarify performative standards that need to be met
+        <br /><br />
 
-      source connects developers to projects and tasks. We work best when both parties can collaborate productively.<br />
-      This document outlines the general guidelines intended to help projects and developers work together effectively, and resolve conflicts should they arise.
-      <br /><br />
+        <Typography variant="h4">For Developers:</Typography>
+        1. Discuss the scope of your work with the project manager<br />
+        2. Start your own branch and complete the task while meeting performative standards agreed upon prior<br />
+        3. Submit a Pull Request when finished
+        <br /><br />
+        <br /><br />
 
-      <Typography variant="h4">For Project Managers:</Typography>
-      1. Specify the scope of tasks, and rewards associated clearly
-      2. Respond to interested parties clearly. Be respectful of their time.
-      3. Follow up on work in progress to clarify performative standards that need to be met
+        <Typography variant="h3">Dispute Resolution</Typography>
+        We generally side with the project if the issue is one of quality. (Doesn't compile, poor documentation etc.)
+        <br />
+        <br />
+        <br />
+        <br />
 
+        <Typography variant="subtitle2">
+        Interested in contributing to our development? Look up our TODOs and reach out to us directly.
+        </Typography>
+        <br />
+        <br />
+        <Typography variant="overline">Contact</Typography>
 
-      <Typography variant="h4">For Developers:</Typography>
-      1. Discuss the scope of your work with the project manager
-      2. Start your own branch and complete the task while meeting performative standards agreed upon prior
-      3. Submit a Pull Request when finished
-
-
-      <Typography variant="h3">Dispute Resolution</Typography>
-      We generally side with the project if the issue is one of quality. (Doesn't compile, poor documentation etc.)
-      <br />
-      <br />
-
-      <Typography variant="subtitle1">Interested in contributing to our development? Look up our TODOs and reach out to us directly.</Typography>
-
-      <Typography variant="h4">Contact</Typography>
-      Twitter: @source_platform
-      <br />
-      Email: source@sourcenetwork.io
-      <br />
-      Telegram: https://t.me/joinchat/EbiqAxL2g2baSz4fulFeHg
+        Twitter:<Link href={"https://www.twitter.com/source_platform"} color="inherit" variant="body2">@source_platform</Link>
+        <br />
+        Email: <Link href={"mailto:source@sourcenetwork.io"} color="inherit" variant="body2">source@sourcenetwork.io</Link>
+        <br />
+        Telegram: <Link href={"https://t.me/joinchat/EbiqAxL2g2baSz4fulFeHg"} color="inherit" variant="body2">https://t.me/joinchat/EbiqAxL2g2baSz4fulFeHg</Link>
+      </div>
     )
   }
   }
