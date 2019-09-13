eea7aab68ea7189f4be72f8e36604c9c117ce777
Lucas <lschlotzerdelucio@outlook.com>
2019-09-13 17:56:56 -0500

    PSQL Production

--- BackEnd/src/db/knex.js
+++ BackEnd/src/db/knex.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "production";
 const config = require("../../knexfile.js")[environment];
 const db = require("knex")(config);
 
